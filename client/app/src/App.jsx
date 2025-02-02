36a590e47821d2e769779eaecd57cf1cf3b9412b
ItsLasa <manojlasantha306@gmail.com>
2025-02-02 12:21:46 +0530

    Initial commit

---
+++ client/app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Book Website</h1>
+
+    <div>
+      <input type="text" placeholder='Book Title' name="" id="" />
+
+      <input type="text" placeholder='Release Date ' name="" id="" />
+    </div>
+    
+    </>
+  )
+}
+
+export default App
